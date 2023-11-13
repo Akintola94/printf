42719bebd17b5b1235ea0fc914e263ec7d44cf46
Akintola94 <akinyeleakintola94@gmail.com>
2023-11-13 08:05:52 -0800

    printf.c

---
+++ g_function.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * g_function - specifies function
+ * @con_spec: conversion specifiers
+ * @args: arguments
+ * Return: char print
+ */
+int get_function(char con_spec, va_list pa)
+{
+	int j = 0;
+	int char_print = 0;
+
+	specifiers_t spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
+		{'d', print_digit},
+		{'i', print_digit},
+		{'r', print_rev_string},
+		{0, NULL}
+	};
+	
+	while (spec[j].specifiers)
+	{
+		if (con_spec == spec[j].specifiers)
+			char_print += spec[j].f(args);
+			j++;
+	}
+
+	if (char_print == 0)
+	{
+		char_print += _putchar('%');
+		char_print += _putchar(con_spec);
+	}
+
+	return (char_print);
+}
--- printf.c
+++ printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - produce output according to a format
@@ -7,10 +8,11 @@
  */
 int _printf(const char *format, ...)
 {
-	int j = 0, char = 0, char_print;
+	int j = 0, char_count = 0, char_print;
 	va_list pa;
 
 	va_start(pa, format);
+
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -22,24 +24,24 @@ int _printf(const char *format, ...)
 		{
 			if (!format[j + 1] || (format[j + 1] == ' ' && !format[j + 2]))
 			{
-				char = -1;
+				char_count = -1;
 				break;
 			}
 			char_print += get_function(format[j + 1], pa);
 			if (char_print == 0)
-				char += _putchar(format[j + 1]);
+				char_count += _putchar(format[j + 1]);
 			if (char_print == -1)
-				char = -1;
+				char_count = -1;
 			j++;
 		}
 		else
 		{
-			(char == -1) ? (_putchar(format[j])) : (char += _putchar(format[j]));
+			(char_count == -1) ? (_putchar(format[j])) : (char_count += _putchar(format[j]));
 		}
 		j++;
-		if (char != -1)
-			char += char_print;
+		if (char_count != -1)
+			char_count += char_print;
 	}
 	va_end(pa);
-	return (char);
+	return (char_count);
 }
